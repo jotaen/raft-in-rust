3ff9ba13c1750e85f4cd39acee7520ed98768dfd
Jan Heuermann <jan@jotaen.net>
2021-05-20 20:44:07 +0200

    Refactor todos

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
-use crate::Message::{Acknowledge, Reject, AppendLog, RequestLog};
+use crate::Message::{Acknowledge, Reject, AppendLog, RequestLog, RequestVote, VoteNo, VoteYes};
 
 fn main() {
     println!("Hello, world!");
 }
 
+type NodeId = u32;
+type TermId = u64;
+
 struct Node {
-    id: u32,
-    leader_id: u32,
-    term_id: u64,
+    id: NodeId,
+    leader_id: NodeId,
+    term_id: TermId,
+    vote_for_future_term: Option<(TermId, NodeId)>,
 }
 
 #[derive(PartialEq)]
 #[derive(Debug)]
 enum Message {
-    AppendLog { sender_id: u32, term_id: u64 },
-    Acknowledge { sender_id: u32 },
+    AppendLog { sender_id: NodeId, term_id: TermId },
+    Acknowledge { sender_id: NodeId },
     Reject,
-    RequestLog { sender_id: u32 },
+    RequestLog { sender_id: NodeId },
+    RequestVote { sender_id: NodeId, proposed_term_id: TermId },
+    VoteNo {sender_id: NodeId },
+    VoteYes {sender_id: NodeId }
 }
 
 impl Node {
@@ -34,30 +41,58 @@ impl Node {
                     Reject
                 }
             }
+            RequestVote{ sender_id, proposed_term_id } => {
+                if proposed_term_id < self.term_id {
+                    return VoteNo { sender_id: self.id }
+                }
+                return match self.vote_for_future_term {
+                    Some((term_id, node_id)) => {
+                        if proposed_term_id > term_id {
+                            return self.save_vote(proposed_term_id, sender_id);
+                        }
+                        if node_id == sender_id {
+                            VoteYes { sender_id: self.id }
+                        } else {
+                            VoteNo { sender_id: self.id }
+                        }
+                    }
+                    None => {
+                        self.save_vote(proposed_term_id, sender_id)
+                    }
+                }
+            }
             _ => {
                 Reject
             }
         };
     }
+
+    fn save_vote(&mut self, proposed_term_id: TermId, node_id: NodeId) -> Message {
+        self.vote_for_future_term = Some((proposed_term_id, node_id));
+        return VoteYes { sender_id: self.id }
+    }
 }
 
-fn new_follower(id: u32, leader_id: u32, term_id: u64) -> Node {
-    return Node { id, leader_id, term_id };
+fn new_follower(id: NodeId, leader_id: NodeId, term_id: TermId) -> Node {
+    return Node { id, leader_id, term_id, vote_for_future_term: Some((term_id, leader_id)) };
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::Message::{AppendLog, Reject, RequestLog};
+    use crate::Message::{AppendLog, Reject, RequestLog, VoteNo, RequestVote, VoteYes};
 
     struct MessageBuilder {
-        id: u32,
-        term_id: u64,
+        id: NodeId,
+        term_id: TermId,
     }
 
     impl MessageBuilder {
         fn append_log(&self) -> Message {
             AppendLog { sender_id: self.id, term_id: self.term_id }
+        }
+        fn request_vote(&self) -> Message {
+            RequestVote { sender_id: self.id, proposed_term_id: self.term_id }
         }
     }
 
@@ -65,7 +100,7 @@ mod tests {
         return MessageBuilder { id: 827349, term_id: 7812635 };
     }
 
-    fn message_builder(id: u32, term_id: u64) -> MessageBuilder {
+    fn message_builder(id: NodeId, term_id: TermId) -> MessageBuilder {
         return MessageBuilder { id, term_id };
     }
 
@@ -114,12 +149,61 @@ mod tests {
     #[test]
     fn test_updates_state_if_term_is_behind() {
         let leader = default_message_builder();
-        let new_leader_in_new_term = message_builder(leader.id + 1, leader.term_id + 1);
+        let new_leader_in_new_term = message_builder(3142798, leader.term_id + 1);
         let mut follower = new_follower(15, leader.id, leader.term_id);
 
         let _result = follower.receive(new_leader_in_new_term.append_log());
         assert_eq!(follower.leader_id, new_leader_in_new_term.id);
         assert_eq!(follower.term_id, new_leader_in_new_term.term_id);
+    }
+
+    #[test]
+    fn test_vote_for_past_term() {
+        let leader = default_message_builder();
+        let leader_in_old_term = message_builder(leader.id, leader.term_id - 1);
+        let mut follower = new_follower(15, leader.id, leader.term_id);
+
+        let result = follower.receive(leader_in_old_term.request_vote());
+        assert_eq!(result, VoteNo { sender_id: follower.id });
+    }
+
+    #[test]
+    fn test_votes_yes_for_future_term_for_first_node_no_for_second_even_if_replayed() {
+        let prev_leader = default_message_builder();
+        let mut follower = new_follower(15, prev_leader.id, prev_leader.term_id);
+
+        let candidate_a = message_builder(987234, prev_leader.term_id + 1);
+        let result = follower.receive(candidate_a.request_vote());
+        assert_eq!(result, VoteYes { sender_id: follower.id });
+        let result = follower.receive(candidate_a.request_vote());
+        assert_eq!(result, VoteYes { sender_id: follower.id });
+
+        let candidate_b = message_builder(109238, prev_leader.term_id + 1);
+        let result = follower.receive(candidate_b.request_vote());
+        assert_eq!(result, VoteNo { sender_id: follower.id });
+        let result = follower.receive(candidate_b.request_vote());
+        assert_eq!(result, VoteNo { sender_id: follower.id });
+    }
+
+    #[test]
+    fn test_always_votes_yes_for_leader_in_same_term() {
+        let leader = default_message_builder();
+
+        let mut follower = new_follower(15, leader.id, leader.term_id);
+
+        let result = follower.receive(leader.request_vote());
+        assert_eq!(result, VoteYes { sender_id: follower.id });
+    }
+
+    #[test]
+    fn test_votes_no_for_other_candidate_in_same_term() {
+        let leader = default_message_builder();
+        let candidate_a = message_builder(264785, leader.term_id);
+
+        let mut follower = new_follower(15, leader.id, leader.term_id);
+
+        let result = follower.receive(candidate_a.request_vote());
+        assert_eq!(result, VoteNo { sender_id: follower.id });
     }
 
     #[test]
